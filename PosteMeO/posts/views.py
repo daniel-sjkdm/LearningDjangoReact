7d12966ea3f47d742a66d6b3cab57883ddf4d951
DanSjukdom <daniel.py.216@gmail.com>
2020-08-16 17:31:04 -0500

    User is now added as  author by using the request

--- PosteMeO/posts/views.py
+++ PosteMeO/posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .models import Post
@@ -11,10 +13,10 @@ from .serializers import PostSerializer
 
 class PostAPI(APIView):
     authentication_classes = [
-
+        BasicAuthentication
     ]
     permission_classes = [
-        
+        IsAuthenticated
     ]
     def get(self, request):
         posts = Post.objects.all()
@@ -22,11 +24,16 @@ class PostAPI(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
     def post(self, request):
         data = request.data
-        serializer = PostDetailAPI(data=data)
+        serializer = PostSerializer(data=data)
+        print(serializer)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(author=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        print('this is fired')
+        serializer.save(author=self.request.user)
+
 
 
 class PostDetailAPI(APIView):
