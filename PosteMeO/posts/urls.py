07abf73df4029330f9a45e73aed1b92471ecf71c
DanSjukdom <daniel.py.216@gmail.com>
2020-08-16 13:43:53 -0500

    urls for post and post detail

---
+++ PosteMeO/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'posts'
+urlpatterns = [
+    path('api/posts/', views.PostAPI.as_view(), name='posts'),
+    path('api/posts/<int:id>/', views.PostDetailAPI.as_view(), name='post-detail'),
+]
