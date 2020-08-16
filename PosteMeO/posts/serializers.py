334eabcd3216e1c7b0bc09873c19fa10c28f00ba
DanSjukdom <daniel.py.216@gmail.com>
2020-08-16 17:32:07 -0500

    TODO task

--- PosteMeO/posts/serializers.py
+++ PosteMeO/posts/serializers.py
@@ -2,6 +2,9 @@ from .models import Post
 from rest_framework import serializers
 
 
+# TODO:
+# - [ ] Make the author an optional field
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
