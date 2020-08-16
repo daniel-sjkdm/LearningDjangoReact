17b3c5b160da765ea7765b9c5ea5d9d8f70c16cc
DanSjukdom <daniel.py.216@gmail.com>
2020-08-16 17:06:23 -0500

    User and User registration serializers

---
+++ PosteMeO/accounts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'date_joined'
+        ]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'password',
+            'email'
+        ]
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        return user
