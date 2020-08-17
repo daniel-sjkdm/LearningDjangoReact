f72c927fe83b29855c97c79c750c5152e3fa3d55
DanSjukdom <daniel.py.216@gmail.com>
2020-08-17 00:19:44 -0500

    Post like url

--- PosteMeO/posts/urls.py
+++ PosteMeO/posts/urls.py
@@ -6,4 +6,5 @@ app_name = 'posts'
 urlpatterns = [
     path('api/posts/', views.PostAPI.as_view(), name='posts'),
     path('api/posts/<int:id>/', views.PostDetailAPI.as_view(), name='post-detail'),
+    path('api/posts/like/', views.PostLikeAPI.as_view(), name='post-like')
 ]
