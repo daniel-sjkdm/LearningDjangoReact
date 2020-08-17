5569cce536fa1382e9d637c44ced4f1842798706
DanSjukdom <daniel.py.216@gmail.com>
2020-08-17 00:19:09 -0500

    Post like api

--- PosteMeO/posts/views.py
+++ PosteMeO/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework import status
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework_jwt.utils import jwt_get_username_from_payload_handler
 
 from .models import Post
 from .serializers import PostSerializer
@@ -14,9 +15,6 @@ from .serializers import PostSerializer
 class PostAPI(APIView):
     authentication_classes = [
         BasicAuthentication
-    ]
-    permission_classes = [
-        IsAuthenticated
     ]
     def get(self, request):
         posts = Post.objects.all()
@@ -30,9 +28,6 @@ class PostAPI(APIView):
             serializer.save(author=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    def perform_create(self, serializer):
-        print('this is fired')
-        serializer.save(author=self.request.user)
 
 
 
@@ -59,3 +54,26 @@ class PostDetailAPI(APIView):
             post.delete()
         except:
             return Response({"Error": "The post doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostLikeAPI(APIView):
+    authentication_classes = [
+        JSONWebTokenAuthentication
+    ]
+    def post(self, request):
+        post = get_object_or_404(Post, id=request.data['post_id'])
+        user = get_object_or_404(User, username=request.user)
+        if post.liked_by.filter(username=user.username).exists():
+            post.liked_by.remove(user)
+            post.likes -= 1
+            post.save()
+            return Response({"message": "Post was disliked"}, status=status.HTTP_200_OK)
+        post.liked_by.add(user)
+        post.likes += 1
+        post.save()
+        return Response({"message": "Post was liked"}, status=status.HTTP_200_OK)
+
+
+'''
+http -f post http://localhost:8000/posts/api/posts/like/ "Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozLCJ1c2VybmFtZSI6InR5bGVyIiwiZXhwIjoxNTk3NjQyODA0LCJlbWFpbCI6IiIsIm9yaWdfaWF0IjoxNTk3NjM5MjA0fQ.0LZR6VTM4KwMH-5xzXTAd34ljCNaT7LYEQ8G5x6lGWQ" post_id=1
+'''
