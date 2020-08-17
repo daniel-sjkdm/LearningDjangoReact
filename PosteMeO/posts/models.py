1f5756b83d354ffa7f5a9686c6bb632fd1c8fd43
DanSjukdom <daniel.py.216@gmail.com>
2020-08-17 00:20:08 -0500

    Post like model

--- PosteMeO/posts/models.py
+++ PosteMeO/posts/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class PostLike(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     likes = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(User, related_name='like_user', blank=True, through=PostLike)
 
     def __str__(self):
         return self.title
