9057d1a831e030f209cb921f4cea17db82a01b6f
DanSjukdom <daniel.py.216@gmail.com>
2020-08-18 12:13:35 -0500

    jwt url's added

--- PosteMeO/accounts/urls.py
+++ PosteMeO/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 
 app_name='accounts'
@@ -7,5 +8,8 @@ urlpatterns = [
     path('api/accounts/', views.UserAPI.as_view(), name='accounts'),
     path('api/accounts/<str:username>/', views.UserDetailAPI.as_view(), name='accounts-detail'),
     path('register/', views.UserRegisterAPI.as_view(), name='accounts-register'),
-    path('login/', views.UserLoginAPI.as_view(), name='accounts-login')
+    path('login/', views.UserLoginAPI.as_view(), name='accounts-login'),
+    path('api/jwt_token/', obtain_jwt_token, name='jwt'),
+    path('api/jwt_token/refresh/', refresh_jwt_token, name='jwt-refresh'),
+    path('api/jwt_token/verify/', verify_jwt_token, name='jwt-verify'),
 ]
