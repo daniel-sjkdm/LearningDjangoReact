c7edcc214beb75807f3ea5a675e351c1a9fadaed
DanSjukdom <daniel.py.216@gmail.com>
2020-08-29 04:22:17 -0500

    django-extensions was added to INSTALLED_APPS

--- PosteMeO/PosteMeO/settings.py
+++ PosteMeO/PosteMeO/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -139,7 +140,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
     ]
 }
 
